e2d4bd4a61c75e1e0f509b9350b0c96c2c321800
Niko Matsakis <niko@alum.mit.edu>
2012-11-09 15:20:19 -0800

    make reduce-1 more interesting

--- js/src/jit-test/tests/parallelarray/reduce-1.js
+++ js/src/jit-test/tests/parallelarray/reduce-1.js
@@ -3,7 +3,8 @@ load(libdir + "parallelarray-helpers.js");
 function testReduce() {
     function mul(v, p) { return v*p; }
 
-    var array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19];
+    var array = [];
+    for (var i = 1; i <= 1024; i++) array.push(i);
     var expected = array.reduce(mul);
 
     var parray = new ParallelArray(array);
