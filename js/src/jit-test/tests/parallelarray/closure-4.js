f4cd4fac2d373a442b4c69021224c5671a0a2976
Felix S. Klock II <pnkfelix@mozilla.com>
2012-11-29 19:37:28 +0100

    More closure tests attempting to exercise NewCallObject with dynamic slots.

--- js/src/jit-test/tests/parallelarray/closure-4.js
+++ js/src/jit-test/tests/parallelarray/closure-4.js
@@ -2,7 +2,6 @@ function testClosureCreationAndInvocation() {
     var a = [1,2,3,4,5];
     var p = new ParallelArray(a);
     function makeaddv(v) { return function (x) { return x+v; }; };
-    // eta-expansion is (or at least can be) treated as call with unknown target
     var m = p.map(makeaddv, {mode: "par", expect: "success"});
     assertEq(m[1](1), 3); // (\x.x+v){v=2} 1 == 3
     assertEq(m[2](2), 5); // (\x.x+v){v=3} 2 == 5
