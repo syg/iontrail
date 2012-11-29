60f0b0186484eb7606d044bccf1d6e9fc5636756
Niko Matsakis <niko@alum.mit.edu>
2012-11-29 16:47:27 -0500

    Work around limitations of current impl

--- js/src/jit-test/tests/parallelarray/alloc-array.js
+++ js/src/jit-test/tests/parallelarray/alloc-array.js
@@ -3,13 +3,13 @@ load(libdir + "parallelarray-helpers.js");
 function buildSimple() {
 
     assertParallelArrayModesCommute(["seq", "par"], function(m) {
-        return new ParallelArray(256, function(i) {
+        return new ParallelArray([256], function(i) {
             return [i, i+1, i+2, i+3];
         }, m);
     });
 
     assertParallelArrayModesCommute(["seq", "par"], function(m) {
-        return new ParallelArray(256, function(i) {
+        return new ParallelArray([256], function(i) {
             var x = [];
             for (var i = 0; i < 4; i++) {
                 x[i] = i;
@@ -19,7 +19,7 @@ function buildSimple() {
     });
 
     assertParallelArrayModesCommute(["seq", "par"], function(m) {
-        return new ParallelArray(256, function(i) {
+        return new ParallelArray([256], function(i) {
             var x = [];
             for (var i = 0; i < 99; i++) {
                 x[i] = i;
