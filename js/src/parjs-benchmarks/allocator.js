7b51c52ba1de673473846b670887072ca1079d7d
Niko Matsakis <niko@alum.mit.edu>
2012-11-30 16:04:11 -0500

    tweak allocator benchmark to be simpler

--- js/src/parjs-benchmarks/allocator.js
+++ js/src/parjs-benchmarks/allocator.js
@@ -1,28 +1,16 @@
-// Adapted from
-//
-// https://github.com/RiverTrail/RiverTrail/blob/master/examples/mandelbrot/mandelbrot.js
-//
-// which in turn is adapted from a WebCL implementation available at
-//
-// http://www.ibiblio.org/e-notes/webcl/mandelbrot.html
-
 var nc = 30, maxCol = nc*3, cr,cg,cb;
 
 load(libdir + "util.js");
 
-function buildBalancedTree(depth) {
-  if (depth == 0) {
-    return [];
-  } else {
-    return [buildBalancedTree(depth-1), buildBalancedTree(depth-1)];
-  }
+function allocateSomeStuff() {
+  return [{a: 1, b: 2}, {c: 3, d: 4}];
 }
 
 function computeSequentially() {
   result = [];
   for (var r = 0; r < ROWS; r++) {
     for (var c = 0; c < COLS; c++) {
-      result.push(buildBalancedTree(DEPTH));
+      result.push(allocateSomeStuff());
     }
   }
   return result;
@@ -30,14 +18,13 @@ function computeSequentially() {
 
 function computeParallel() {
   return new ParallelArray([ROWS, COLS], function(r, c) {
-    return buildBalancedTree(DEPTH);
+    return allocateSomeStuff();
   }).flatten();
 }
 
 var ROWS  = 1024;
-var COLS  = 512;
+var COLS  = 1024;
 var DEPTH = 2;
 
-// Experimentally, warmup doesn't seem to be necessary:
-benchmark("ALLOCATOR", 1, DEFAULT_MEASURE,
+benchmark("ALLOCATOR", 2, 6,
           computeSequentially, computeParallel);
