26f95d4ae9867d5c4edaa4e8460ace0b84cc6fe7
Niko Matsakis <niko@alum.mit.edu>
2012-12-04 18:27:26 -0800

    temp. add exit status

--- js/src/jit-test/tests/parallelarray/call-recursive-without-base-case.js
+++ js/src/jit-test/tests/parallelarray/call-recursive-without-base-case.js
@@ -1,3 +1,5 @@
+// |jit-test| exitstatus: 3;
+
 function factorial(n) {
     if (n > 10)
         return factorial(n);
@@ -11,7 +13,10 @@ function factorial(n) {
 function testMap() {
     // sneak in a 22 that causes infinite iteration, but only
     // after warmup iters have completed!
-    var p = new ParallelArray([0,1,2,3,4,5,22,6,7,8]);
+  var p = new ParallelArray([0,1,2,3,4,5,22,7,8,
+                             9,10,11,12,13,14,15,
+                             16,17,18,19,20,21,22,23,
+                             24,25,26,27,28,29,30,31]);
     var q = p.map(function(x) { return factorial(x); },
                   { mode: "par", expect: "bail" });
 }
