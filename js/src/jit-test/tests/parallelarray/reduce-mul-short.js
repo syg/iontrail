8f20ad545fc37d2c66d2eeb3662ab2bc3558390c
Shu-yu Guo <shu@rfrn.org>
2012-12-06 23:18:21 -0800

    Update reduce-mul-short test with explanation and passing assert

--- js/src/jit-test/tests/parallelarray/reduce-mul-short.js
+++ js/src/jit-test/tests/parallelarray/reduce-mul-short.js
@@ -12,13 +12,50 @@ function testReduce() {
   var expected = array.reduce(mul);
   print(expected);
 
+  // The expected pattern is the following:
+  //
+  // Success #1, Bailout #1, Bailout #2, Fixpoint Success
+  //
+  // Success #1 is because the overflow doesn't bail out of Ion. I haven't
+  // investigated further for why it doesn't.
+  //
+  // At this point the user function f() gets invalidated due to the following
+  // line in ParallelArrayReduce:
+  //
+  //     a = f(a, subreductions[i]);
+  //
+  // The 2nd argument is frozen (due to f() having been compiled) to be an
+  // int, because in the reduce() helper we have:
+  //
+  //     a = f(a, self.get(i));
+  //
+  // Here, the 2nd argument is always an int because the accumulator is the
+  // one getting the float.
+  //
+  // So after we return from the Success #1, type propagation trips
+  // recompilation of f().
+  //
+  // Bailout #1 happens because the fill function is still compiled and has a
+  // valid parallelIonScript, so no warmup is attempted. We bail when we hit
+  // the CallGeneric to f, which was invalidated after Success #1.
+  //
+  // At this point the *caller* of f(), reduce(), gets invalidated because it
+  // was the innermost script that aborted.
+  //
+  // Bailout #2 happens because now that reduce() got invalidated and has no
+  // parallelIonScript, the fill() function bails on CallGeneric to it.
+  //
+  // Finally, fill() gets invalidated.
+  //
+  // Fixpoint Success is because we re-warmup and recompile with the
+  // up-to-date type info.
   var parray = new ParallelArray(array);
-  var results = ["success", "bailout", "bailout", "bailout"];
+  var results = ["success", "bailout", "bailout", "success", "success"];
+  var actual;
   for (var i = 0; i < results.length; i++)
-    parray.reduce(mul, {mode: "par", expect: results[i]});
+    actual = parray.reduce(mul, {mode: "par", expect: results[i]});
 
-  // Can we ever get this to work? or does the pattern of bailouts cause issues?
-  compareAgainstArray(array, "reduce", sum);
+  assertAlmostEq(actual, expected);
 }
 
 testReduce();
