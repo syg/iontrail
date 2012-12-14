8f928fec17ed9daa10a4c1f39da1a1ef11a08e70
Niko Matsakis <niko@alum.mit.edu>
2012-12-14 15:40:20 -0800

    fix map-short test

--- js/src/jit-test/tests/parallelarray/map-short.js
+++ js/src/jit-test/tests/parallelarray/map-short.js
@@ -1,11 +1,11 @@
+load(libdir + "parallelarray-helpers.js");
+
 function test() {
   // Test what happens if the length of the array is very short (i.e.,
   // less than the number of cores).  There used to be a bug in this
   // case that led to crashes or other undefined behavior.
-  var a = [1,2];
-  var p = new ParallelArray(a);
   var makeadd1 = function (v) { return [v]; }
-  p.map(makeadd1, {mode: "par", expect: "disqualified"});
+  compareAgainstArray(range(1, 3), "map", makeadd1);
 }
 
 test();
