22771f1c039a9438082dff402b7d7805b5f8f469
Shu-yu Guo <shu@rfrn.org>
2013-01-07 16:17:15 -0800

    Revert "Ensure callsite cloning is required when TI is on"
    This reverts commit 158221a8d18f2d29292fbb966e29ad4300147b0a.
    
    Conflicts:
    	js/src/methodjit/InvokeHelpers.cpp

--- js/src/jscntxt.cpp
+++ js/src/jscntxt.cpp
@@ -291,7 +291,6 @@ IsCloneSpewActive(CloneSpewChannel channel)
 RawFunction
 js::CloneFunctionAtCallsite(JSContext *cx, HandleFunction fun, HandleScript script, jsbytecode *pc)
 {
-    JS_ASSERT(cx->typeInferenceEnabled());
     JS_ASSERT(fun->isCloneAtCallsite());
     JS_ASSERT(types::UseNewTypeForClone(fun));
     JS_ASSERT(!fun->nonLazyScript()->enclosingStaticScope());
