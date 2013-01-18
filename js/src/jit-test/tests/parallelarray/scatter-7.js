29c649b2fb58a9ceb0fec6d2df8acec9cd4c1d16
Niko Matsakis <niko@alum.mit.edu>
2013-01-18 11:42:20 -0800

    reduce size of the arrays to lower test running time

--- js/src/jit-test/tests/parallelarray/scatter-7.js
+++ js/src/jit-test/tests/parallelarray/scatter-7.js
@@ -3,7 +3,7 @@ load(libdir + "parallelarray-helpers.js");
 function testScatterIdentity() {
   var shape = [5];
   for (var i = 0; i < 7; i++) {
-    shape.push(i+1);
+    shape.push(2);
     var p = new ParallelArray(shape, function(k) { return k; });
     var r = p.scatter([0,1,2,3,4]);
     var p2 = new ParallelArray([p.get(0), p.get(1), p.get(2), p.get(3), p.get(4)]);
