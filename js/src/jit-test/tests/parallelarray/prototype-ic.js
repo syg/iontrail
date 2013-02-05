847e015cb172eb8bec13b078654466c491c02064
Shu-yu Guo <shu@rfrn.org>
2013-02-04 18:32:17 -0800

    Change call-to-method-mut to an IC test

--- js/src/jit-test/tests/parallelarray/prototype-ic.js
+++ js/src/jit-test/tests/parallelarray/prototype-ic.js
@@ -10,24 +10,15 @@ MyClass.prototype.getA = function() {
 
 function testMap() {
   var instances = range(22, 22+64).map(function (i) { return new MyClass(i); });
-
-  // first time, everything goes well.
   var p = new ParallelArray(instances);
-  var q = p.map(function(e) { return e.getA(); },
-                {mode: "par", expect: "success"});
-  print(q);
-  assertEq(q.get(0), 22);
-  assertEq(q.get(1), 23);
 
-  // second time, we mutate getA().  This should trigger a
-  // recompilation which will fail because the call has multiple
-  // observed targets.
+  // ICs should work.
   MyClass.prototype.getA = function() {
     return 222;
   };
 
   var r = p.map(function(e) { return e.getA(); },
-                {mode: "par", expect: "disqualified"});
+                {mode: "par", expect: "success"});
   print(r);
   assertEq(r.get(0), 222);
   assertEq(r.get(1), 222);
