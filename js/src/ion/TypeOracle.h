8785f33bbc5dca69c389fb57fb33f0ca7c6e1237
Niko Matsakis <niko@alum.mit.edu>
2013-02-13 11:35:35 -0800

    Gah, accidental diff

--- js/src/ion/TypeOracle.h
+++ js/src/ion/TypeOracle.h
@@ -1,4 +1,4 @@
-n/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
  * vim: set ts=4 sw=4 et tw=99:
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
