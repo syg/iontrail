b76965f59e3d4402de74a1e6402b5f8efc754c89
Niko Matsakis <niko@alum.mit.edu>
2013-02-28 12:42:17 -0800

    update name to be more consistent

--- js/src/parjs-benchmarks/edgesParallelArray2D.js
+++ js/src/parjs-benchmarks/edgesParallelArray2D.js
@@ -1,5 +1,5 @@
 load(libdir + "edges.js");
 
-benchmark("EDGES-2D", 1, DEFAULT_MEASURE,
+benchmark("EDGES-PARALLEL-ARRAY-2D", 1, DEFAULT_MEASURE,
           function() {edges1dArraySequentially(ArrInput, Width, Height)},
           function() {edges2dParallelArrayInParallel(ParArrInput2D)});
