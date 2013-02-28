4196880d2500ae656ff1b80e98ff875d8837ceb0
Niko Matsakis <niko@alum.mit.edu>
2013-02-28 12:58:30 -0800

    Modify the address computations to be consistent with how PA will do it (not that it matters, really), and add in (commented out) hand-inlined, idealized versions of get for comparison purposes.

--- js/src/parjs-benchmarks/edges.js
+++ js/src/parjs-benchmarks/edges.js
@@ -61,7 +61,7 @@ function edges1dArrayInParallel(data, width, height) {
         for (var offX = -1; offX <= 1; offX++) {
           var newX = x + offX;
           if ((newX >= 0) && (newX < width) && (newY >= 0) && (newY < height)) {
-            var pointIndex = (x + offX + (y + offY) * width);
+            var pointIndex = (x + offX) * height + (y + offY);
             var e = data[pointIndex];
             totalX += e * sobelX[offY + 1][offX + 1];
             totalY += e * sobelY[offY + 1][offX + 1];
@@ -92,7 +92,8 @@ function edges1dParallelArrayInParallel(pa, width, height) {
         for (var offX = -1; offX <= 1; offX++) {
           var newX = x + offX;
           if ((newX >= 0) && (newX < width) && (newY >= 0) && (newY < height)) {
-            var pointIndex = (x + offX + (y + offY) * width);
+            var pointIndex = (x + offX) * height + (y + offY);
+            // var e = pa.buffer[pointIndex];
             var e = pa.get(pointIndex);
             totalX += e * sobelX[offY + 1][offX + 1];
             totalY += e * sobelY[offY + 1][offX + 1];
@@ -126,7 +127,8 @@ function edges2dParallelArrayInParallel(pa) {
         for (var offX = -1; offX <= 1; offX++) {
           var newX = x + offX;
           if ((newX >= 0) && (newX < width) && (newY >= 0) && (newY < height)) {
-            var pointIndex = (x + offX + (y + offY) * width);
+            var pointIndex = (x + offX) * height + (y + offY);
+            // var e = pa.buffer[pointIndex];
             var e = pa.get(x + offX, y + offY);
             totalX += e * sobelX[offY + 1][offX + 1];
             totalY += e * sobelY[offY + 1][offX + 1];
