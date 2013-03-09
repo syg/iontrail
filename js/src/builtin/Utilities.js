ead0ae2625245b2e5d33809249e9e0c707b10fc3
Shu-yu Guo <shu@rfrn.org>
2013-03-08 19:12:43 -0800

    Fix typo

--- js/src/builtin/Utilities.js
+++ js/src/builtin/Utilities.js
@@ -18,7 +18,7 @@
          JSMSG_EMPTY_ARRAY_REDUCE: false,
 */
 
-/* Build the message table using a macro-definining macro. */
+/* Build the message table using a macro-defining macro. */
 #defmeta MSG_DEF(name, id, _, _, _) \
   #define name id
 #include ../js.msg
