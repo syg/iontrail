f3500930426944c43c950a6eb9b097cf74d7b8c1
Shu-yu Guo <shu@rfrn.org>
2013-05-29 20:38:49 -0700

    Fix StringPolicy to add MToString for doubles

--- js/src/ion/TypePolicy.cpp
+++ js/src/ion/TypePolicy.cpp
@@ -295,7 +295,7 @@ StringPolicy<Op>::staticAdjustInputs(MInstruction *def)
         return true;
 
     MInstruction *replace;
-    if (in->type() == MIRType_Int32) {
+    if (in->type() == MIRType_Int32 || in->type() == MIRType_Double) {
         replace = MToString::New(in);
     } else {
         if (in->type() != MIRType_Value)
